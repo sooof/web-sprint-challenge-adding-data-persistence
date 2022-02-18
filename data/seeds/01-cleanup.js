d4421c1bd97b0e4e99ecabf6e64b29ac544ec546
Mingjun Geng <sooofeve@gmail.com>
2022-02-18 15:53:09 -0800

    seed work

--- data/seeds/01-cleanup.js
+++
@@ -1,8 +0,0 @@
-const cleaner = require('knex-cleaner')
-
-exports.seed = function(knex) {
-  return cleaner.clean(knex, {
-    mode: 'truncate',
-    ignoreTables: ['knex-migrations', 'knex_migrations_lock'],
-  })
-};
